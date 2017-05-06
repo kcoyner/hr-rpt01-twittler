e0ae219e6b44fc399665f3b417853ad647330c2f
Kevin Coyner <kevin@rustybear.com>
2017-05-06 09:43:47 -0400

    doodle: Added refresh button that doesn't do anything yet.

--- src/js/twittler.js
+++ src/js/twittler.js
@@ -16,12 +16,16 @@ $(document).ready(function(){
   var $body = $('body');
   $body.html('');
 
+  var $button = $('<button type="button">Refresh</button>');
+  $button.prependTo($body);
+
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
     var $tweet = $('<div></div>');
     $tweet.text('@' + tweet.user + ': ' + tweet.message);
     $tweet.appendTo($body);
+
     index -= 1;
   }
 
