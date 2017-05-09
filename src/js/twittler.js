fb03ef1d2a62a8aaf41cd69d94a79631243bbcad
Kevin Coyner <kevin@rustybear.com>
2017-05-09 00:01:17 -0400

    doodle: Progress capture.

--- src/js/twittler.js
+++ src/js/twittler.js
@@ -13,20 +13,55 @@ Allow the user to click on a username to see that user's timeline.
 */
 
 $(document).ready(function(){
-  var $body = $('body');
-  $body.html('');
+  // var $body = $('body');
+  // $body.html('');
+  // refreshTwits();
+  buildTwits();
+  var $twitColumnHead = document.getElementById('twitColumnHead');
+  // var $refreshTwits = $('<button type="button" id="refreshButton"></button>');
+  // $refreshTwits.prependTo($twitColumnHead);
+  // var $clearTwits = $('<button type="button" id="clearTwits"></button>');
+  // $clearTwits.prependTo($twitColumnHead);
 
-  var $button = $('<button type="button">Refresh</button>');
-  $button.prependTo($body);
+  var $updateButton = $('<button type="button" id="updateButton">Update</button>');
+  $updateButton.prependTo($twitColumnHead);
+  $('#updateButton').on('click', function() {
+    buildTwits();
+  });
 
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($body);
+  var $clearButton = $('<button type="button" id="clearButton">Clear</button>');
+  $clearButton.prependTo($twitColumnHead);
+  $('#clearButton').on('click', function() {
+    clearTwits();
+    // $('#clearButton').text('back home');
+  });
 
-    index -= 1;
-  }
 
 });
+
+var buildTwits = function() {
+  var $twitColumn = document.getElementById('twitColumn');
+  var index = streams.home.length - 1;
+  while(index >= 8){
+    var tweet = streams.home[index];
+    var $twitTime = tweet.created_at.toLocaleTimeString();
+    var $tweet = $('<div .twits></div>');
+    $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ::   ' +  $twitTime );
+    $tweet.appendTo($twitColumn);
+    index -= 1;
+  }
+};
+
+function clearTwits() {
+  var $twitColumn = document.getElementById('twitColumn');
+  window.location.reload(true);
+}
+
+
+// function setUser(){
+//   var clickedUser=$(this).text();
+//   window.watchedUser = clickedUser;
+//   state = !state;
+//   buildTwits();
+// }
+
