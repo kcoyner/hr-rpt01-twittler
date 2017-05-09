ec7af404d9b1192f1194740f225e9f6f651d526d
Kevin Coyner <kevin@rustybear.com>
2017-05-09 15:16:16 -0400

    doodle: Working. Could use refactoring and styling.

--- src/js/twittler.js
+++ src/js/twittler.js
@@ -10,46 +10,27 @@ Design your interface so that you want to look at and use the product you're
 making.
 
 Allow the user to click on a username to see that user's timeline.
-*/
-
-$(document).ready(function(){
-
-  // buildTwits();
-  var $twitColumnHead = document.getElementById('twitColumnHead');
-  // var $refreshTwits = $('<button type="button" id="refreshButton"></button>');
-  // $refreshTwits.prependTo($twitColumnHead);
-  // var $clearTwits = $('<button type="button" id="clearTwits"></button>');
-  // $clearTwits.prependTo($twitColumnHead);
-
-  var $updateButton = $('<button type="button" id="updateButton">Update</button>');
-  $updateButton.prependTo($twitColumnHead);
-  $('#updateButton').on('click', function() {
-    // clearTwits();
-    // window.location.reload(true);
-    // buildTwits();
-    buildTweets();
-    renderTweets();
-  });
-
-  var $clearButton = $('<button type="button" id="clearButton">Clear</button>');
-  $clearButton.prependTo($twitColumnHead);
-  $('#clearButton').on('click', function() {
-    clearTwits();
-    // $('#clearButton').text('back home');
-  });
-  buildTweets();
-  renderTweets();
-
-
-});
 
 // streams.home is an array of twit objects {created_at, message, user}
 // window.users is an array of user names (strings)
 // streams.users is an object { user: [the users messages as {}] }
 
-var tweetArray = [];
+*/
 
+$(document).ready(function(){
+  var $tweetColumnHead = document.getElementById('tweetColumnHead');
+
+  var $updateButton = $('<button type="button" id="updateButton">Update</button>');
+  $updateButton.prependTo($tweetColumnHead);
+  $('#updateButton').on('click', function() {
+    buildTweets();
+    renderTweets();
+  });
+
+
+/* Receive tweets from data-generator, sort the tweets, and tightly control them.  */
 var buildTweets = function() {
+  tweetArray = [];
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
@@ -61,85 +42,84 @@ var buildTweets = function() {
   return tweetArray;
 };
 
-// var buildTweets = function() {
-//   // var index = streams.home.length - 1;
-//   // var $twitTime = '';
-//   var index = tweetArray.length - 1;
-//   while(index >= 0){
-//     var tweet = streams.home[index];
-//     // $twitTime = tweet.created_at.toLocaleTimeString();
-//     var $tweet = $('<div .twits></div>');
-//     $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ::   ' +  tweet.created_at );
-//     console.log('tweet.: ', tweet.created_at);
-//     tweetArray.push($tweet);
-//     index -= 1;
-//   }
-//   sortTweets(tweetArray, 'created_at');
-//   // console.log('tweetArray: ', tweetArray[0]);
-// };
-
-var renderTweets = function() {
-  var $twitColumn = document.getElementById('twitColumn');
-  $(twitColumn).empty();
+/* Take the tweets received from buildTweets() and display at most 15 at a time. 
+ * TODO: add user setting to allow choices on the number of tweets displayed.
+ */
+var renderTweets = function(){
+  var $tweetColumn = document.getElementById('tweetColumn');
+  $(tweetColumn).empty();
   tweetArray = tweetArray.slice(0, 15);
-  // var index = tweetArray.length - 1;
-  // var index = tweetArray.slice(0,20);
-  // var $tweet = '';
+
   tweetArray.forEach((tweet,idx) => {
     tweet = tweetArray[idx];
-    var $twitTime = tweet.created_at.toLocaleTimeString();
-    var $tweet = $('<div .twits></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ::   ' +  $twitTime );
-    $tweet.appendTo($twitColumn);
+    $tweetBox = makeTweetBox();
+    
+    var $user = $('<span style="color:blue" class="user">???</span>');
+    tweet.user = `${tweet.user}`;
+    $user.text(tweet.user);
+    $user.click(setUser);
+    $tweetBox.append($user);
 
+    var $tweetMsg = $('<span class="tweetMsg">???</span>');
+    $tweetMsg.text(' -- ' + tweet.message);
+    $tweetBox.append($tweetMsg);
+
+    var $tweetTime = $('<span class="tweetTime">???</span>');
+    $tweetTime.text(' :: ' + tweet.created_at.toLocaleTimeString());
+    $tweetBox.append($tweetTime);
+
+    $tweetBox.appendTo($tweetColumn);
   });
-
-  // while(index >= 0){
-  //   var tweet = tweetArray[index];
-  //   var $twitTime = tweet.created_at.toLocaleTimeString();
-  //   var $tweet = $('<div .twits></div>');
-  //   $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ::   ' +  $twitTime );
-  //   // console.log('tweetArray: ', tweetArray);
-  //   // $tweet = tweetArray[index];
-  //   $tweet.appendTo($twitColumn);
-  //   index -= 1;
-  // }
-
 };
 
+/* Display tweets filtered by user/author */
+var renderUserTweets = function(clickedUser){
+  var $tweetColumn = document.getElementById('tweetColumn');
+  $(tweetColumn).empty();
 
-// // streams.home is an array of twit objects {created_at, message, user}
-// var buildTwits = function() {
-//   var $twitColumn = document.getElementById('twitColumn');
-//   var index = streams.home.length - 1;
-//   $(twitColumn).empty();
-//   streams.home = sortTweets(streams.home, 'created_at');
-//   while(index >= 8){
-//     var tweet = streams.home[index];
-//     var $twitTime = tweet.created_at.toLocaleTimeString();
-//     var $tweet = $('<div .twits></div>');
-//     $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ::   ' +  $twitTime );
-//     $tweet.appendTo($twitColumn);
-//     index -= 1;
-//   }
-// };
+  tweetArray = tweetArray.filter(tweet => tweet.user === clickedUser);
 
-function clearTwits() {
-  var $twitColumn = document.getElementById('twitColumn');
-  $(twitColumn).empty();
-  // window.location.reload(true);
-}
+  tweetArray.forEach((tweet,idx) => {
+    tweet = tweetArray[idx];
+    $tweetBox = makeTweetBox();
+    
+    var $user = $('<span style="color:blue" class="user">???</span>');
+    tweet.user = `${tweet.user}`;
+    $user.text(tweet.user);
+    $tweetBox.append($user);
 
-function sortTweets(array) {
+    var $tweetMsg = $('<span class="tweetMsg">???</span>');
+    $tweetMsg.text(' -- ' + tweet.message);
+    $tweetBox.append($tweetMsg);
+
+    var $tweetTime = $('<span class="tweetTime">???</span>');
+    $tweetTime.text(' :: ' + tweet.created_at.toLocaleTimeString());
+    $tweetBox.append($tweetTime);
+
+    $tweetBox.appendTo($tweetColumn);
+  });
+};
+
+var makeTweetBox = function(){
+    var $tweetBox = $('<div class="tweets"></div>');
+    var $atSign = $('<span style="color:blue">???</span>');
+    $atSign.text('@');
+    $tweetBox.append($atSign);
+  return $tweetBox;
+};
+
+var sortTweets = function(array){
   return array.sort((a, b) => {
     return b.created_at - a.created_at;
   });
-}
+};
 
-// function setUser(){
-//   var clickedUser=$(this).text();
-//   window.watchedUser = clickedUser;
-//   state = !state;
-//   buildTwits();
-// }
+var setUser = function(){
+  var clickedUser=$(this).text();
+  renderUserTweets(clickedUser);
+};
 
+buildTweets();
+renderTweets();
+
+});
